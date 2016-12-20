e9f8af15296e2d3749e8b68e52aad3f353af48a8
mrfosse <mrfosse99@gmail.com>
2016-12-19 16:32:05 -0800

    removed extra lib stuff

--- shell.h
+++ shell.h
@@ -9,11 +9,6 @@
 #include <string.h>
 #include <sys/wait.h>
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
-
-
 /**
  * struct builtin_t - stores pointers to builtin functions
  */
